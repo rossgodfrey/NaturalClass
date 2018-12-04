0d17be18945e079c3ecb41446ee7bf135ddf8124
Tomo <tomyoko@users.noreply.github.com>
2018-12-03 22:16:22 -0500

    created classes including Deck constructors, also functions to deal cards

--- natural_class.py
+++ natural_class.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 def is_set(cards):  
   features = ["shading", "color", "number", "shape"]
   for feature in features:
@@ -8,3 +10,48 @@ def is_set(cards):
       if 1 < len(unique_values) < len(cards):
         return False              
   return True
+
+class Game:
+	def __init__(self, deck, players):
+		self.deck = deck
+		self.players = players
+
+class Deck:
+	def __init__(self):
+		self.undealt_cards = []
+		shadings = ["solid", "striped", "empty"]
+		colors	= ["red", "green", "purple"]
+		shapes	= ["oval", "diamond", "squiggle"]
+		numbers	= ["one", "two", "three"]
+		for shading in shadings:
+			for color in colors:
+				for shape in shapes:
+					for number in numbers:
+						self.undealt_cards.append(Card(shading, color, shape, number))
+		shuffle(self.undealt_cards)
+		self.cards_in_play = []
+
+	def deal(self, number_of_cards):
+		for _ in range(number_of_cards):
+			self.cards_in_play.append(self.undealt_cards.pop())
+
+class Player:
+	def __init__(self, score, name):
+		self.score = score
+		self.name	= name
+
+class Card:
+	def __init__(self, shading, color, shape, number):
+		self.shading = shading
+		self.color = color
+		self.shape = shape
+		self.number = number
+	def __repr__(self):
+		return f"({self.shading}, {self.color}, {self.shape}, {self.number})"
+
+if __name__ == "__main__":
+	d = Deck()
+	d.deal(12)
+	print(len(d.undealt_cards))
+	print(len(d.cards_in_play))
+
